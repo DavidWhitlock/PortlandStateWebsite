3770df044aceb78f83222b4f09884d84e4bcd764
David Whitlock <david.m.whitlock@gmail.com>
2010-06-12 15:39:36 +0000

    Added the Java Review lecture to the class schedule

--- js/schedule.js
+++ js/schedule.js
@@ -55,10 +55,16 @@ function populateSchedule(schedule) {
   while (div.hasChildNodes()) {
     div.removeChild(div.firstChild);
   }
-  
+
   startDate = Date.parseString(schedule.meetings.first()).getTime();
   endDate = Date.parseString(schedule.meetings.last()).getTime();
 
+  // Add the information about the review of Java
+  if (schedule.review) {
+    reviewInformation(div, hasDatePassed(startDate), schedule.review);
+  }
+  
+  
   // Don't show dates 30 days after the course has ended
   var showDate = now < endDate + (30 * 24 * 60 * 60 * 1000);
 
@@ -66,58 +72,15 @@ function populateSchedule(schedule) {
     var meeting = Date.parseString(schedule.meetings[i]);
     var hasPassed = hasDatePassed(meeting);
 
-    var dl = $(document.createElement("dl"));
-    div.appendChild(dl);
-
-    var dt = $(document.createElement("dt"));
-    dl.appendChild(dt);
-
-    var img = $(document.createElement("img"));
-    if (hasPassed) {
-      img.src = "img/collapsed.gif";
-    } else {
-      img.src = "img/expanded.gif";
-    }
-    img.onclick = function() {
-      var siblings = this.parentNode.siblings();
-      if (siblings[0].visible()) {
-        this.src = "img/collapsed.gif";
-
-      } else {
-        this.src = "img/expanded.gif";
-      }
-
-      siblings.invoke('toggle');
-    };
-
-    dt.appendChild(img);
-
-    appendTextChild(dt, " Week " + (i+1) + (showDate ? ": " + meeting.format("MMM d, yyyy") : ""));
+    var dl = lectureHeading(div, hasPassed, " Week " + (i+1) + (showDate ? ": " + meeting.format("MMM d, yyyy") : ""));
 
     var lecture = schedule.lectures[i];
     if (lecture.comment) {
-        var dd = $(document.createElement("dd"));
-        if (hasPassed) {
-          dd.hide();
-        }
-        dl.appendChild(dd);
-        var ital = $(document.createElement("i"));
-        dd.appendChild(ital);
-        ital.innerHTML = lecture.comment;
+      lectureComment(dl, hasPassed, lecture.comment)
     }
 
     if (lecture.content) {
-      var ul = $(document.createElement("ul"));
-      if (hasPassed) {
-        ul.hide();
-      }
-      dl.appendChild(ul);
-
-      for (var j = 0; j < lecture.content.length; j++) {
-        var li = $(document.createElement("li"));
-        ul.appendChild(li);
-        li.innerHTML = lecture.content[j];
-      }
+      lectureContent(dl, hasPassed, lecture.content);
     }
   }
 }
@@ -127,4 +90,80 @@ function populateSchedule(schedule) {
  */
 function appendTextChild(parent, text) {
   parent.appendChild(document.createTextNode(text));
+}
+
+/**
+ * Outputs HTML for the heading for a lecture
+ */
+function lectureHeading(parent, hasPassed, heading) {
+  var dl = $(document.createElement("dl"));
+  parent.appendChild(dl);
+
+  var dt = $(document.createElement("dt"));
+  dl.appendChild(dt);
+
+  var img = $(document.createElement("img"));
+  if (hasPassed) {
+    img.src = "img/collapsed.gif";
+  } else {
+    img.src = "img/expanded.gif";
+  }
+  img.onclick = function() {
+    var siblings = this.parentNode.siblings();
+    if (siblings[0].visible()) {
+      this.src = "img/collapsed.gif";
+
+    } else {
+      this.src = "img/expanded.gif";
+    }
+
+    siblings.invoke('toggle');
+  };
+
+  dt.appendChild(img);
+
+  appendTextChild(dt, heading);
+
+  return dl;
+}
+
+/**
+ * Outputs HTML for a comment about a lecture
+ */
+function lectureComment(parent, hasPassed, comment) {
+  var dd = $(document.createElement("dd"));
+  if (hasPassed) {
+    dd.hide();
+  }
+  parent.appendChild(dd);
+  var ital = $(document.createElement("i"));
+  dd.appendChild(ital);
+  ital.innerHTML = comment;
+}
+
+/**
+ * Outputs HTML for the content of a lecture
+ */
+function lectureContent(parent, hasPassed, content) {
+  var ul = $(document.createElement("ul"));
+  if (hasPassed) {
+    ul.hide();
+  }
+  parent.appendChild(ul);
+
+  for (var j = 0; j < content.length; j++) {
+    var li = $(document.createElement("li"));
+    ul.appendChild(li);
+    li.innerHTML = content[j];
+  }
+}
+
+/**
+ * Outputs HTML about the review work that should be done before class starts
+ */
+function reviewInformation(parent, hasPassed, review) {
+  var dl = lectureHeading(parent, hasPassed, " Before class starts: Brush up on your Java");
+
+  lectureComment(dl, hasPassed, review.comment);
+  lectureContent(dl, hasPassed, review.content);
 }
